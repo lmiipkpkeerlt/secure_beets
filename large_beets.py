2dbb41294ccc1c9fad91a94b5a1a0ebfc5830d28
amav <amav@itu.dk>
2022-05-16 17:31:02 +0200

    large input files

--- large_beets.py
+++ large_beets.py
@@ -1,10 +1,10 @@
 import random
 
-f = open("player0.txt", "w")
-g = open("player1.txt", "w")
-k = open("player2.txt", "w")
+f = open("player0ll.txt", "w")
+g = open("player1ll.txt", "w")
+k = open("player2ll.txt", "w")
 
-for i in range(150):
+for i in range(4500):
     n = random.randint(50,100)
     f.write('100 ' + str(n) + " ")
     n = random.randint(50,100)
@@ -12,7 +12,7 @@ for i in range(150):
     n = random.randint(100,350)
     k.write('100 ' + str(n) + " ")
 
-for i in range(150):
+for i in range(4500):
     b = random.randint(70,150)
     f.write('200 ' + str(b) + " ")
     b = random.randint(70,150)
@@ -20,7 +20,7 @@ for i in range(150):
     b = random.randint(130,300)
     k.write('200 ' + str(b) + " ")
 
-for i in range(150):
+for i in range(4500):
     c = random.randint(100,200)
     f.write('300 ' + str(c) + " ")
     c = random.randint(100,200)
